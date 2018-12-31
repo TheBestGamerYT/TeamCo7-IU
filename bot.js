4feb8c23ffedd926d2d152de4f8da795320273ba
MarkFuljhari <45743552+MarkFuljhari@users.noreply.github.com>
2018-12-30 23:16:35 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,12 +1,12 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-var prefix = "!c7 "
+var prefix = "!iu "
 
 client.on('ready',() => {
   console.log("I\'m Online !");
-  client.user.setActivity('!c7 help | discord.me/teamco7', { type: 'WATCHING' })
-    .then(presence => console.log(`Activity set to ${presence.game ? presence.game.name : '!c7 help | discord.gg/67gbBKH'}`))
+  client.user.setActivity('TeamCo7 servers | !iu help', { type: 'WATCHING' })
+    .then(presence => console.log(`Activity set to ${presence.game ? presence.game.name : 'TeamCo7 servers | !iu help'}`))
     .catch(console.error);
 })
 
@@ -14,10 +14,10 @@ client.on('message', message => {
 
   if (message.author === client.user) return;
   	if (message.content.startsWith(prefix + 'ping')) {
-  		message.channel.send('pong! I can\'t know the ping. I am still in BETA version \n Developers still have to complete this discord.js code');
+  		message.channel.send('pong! I can\'t know the ping. IU is still in BETA version! \n But we still hunt!');
   	}
     if (message.content.startsWith(prefix + 'help')) {
-  		message.channel.send('Bot Help Message: \n !c7 ping - pong \n !c7 help - help message \n This bot is still in BETA version by TeamCo7 Clan!  \n If our host gets DDoS hacked, then please stay calm and wait for response from TeamCo7 Clan \n Discord server: https://discord.gg/67gbBKH');
+  		message.channel.send('Hi! This bot belongs to TeamCo7 Clan Inspection Unit (IU) \n An authority in charge of hunting cheaters, hackers, rule breakers & corrupt groups \n !iu ping - pong \n !iu help - This message \n Discord server: https://discord.gg/67gbBKH');
   	}
 });
 //  
